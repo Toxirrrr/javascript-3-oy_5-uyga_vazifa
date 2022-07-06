43cb264570071e7e416ee40d97b2c32848f2d652
Toxirrrr <mjuraeva74@gmail.com>
2022-07-06 16:16:31 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var myByudjet = prompt("Pulni kiriting");
+
+var dollar = 9433.34;
+
+var evro = 10354.03;
+
+var byudjet = Math.floor((dollar * 800) + (evro * 150));
+
+if (myByudjet >= byudjet) {
+    console.log("Oq Yol Alisher");
+    alert("Oq Yol ALisher");
+} else {
+    console.log("Alisher Ozgina Sabr qiling ");
+    alert("Alisher Ozgina Sabr qiling");
+};
